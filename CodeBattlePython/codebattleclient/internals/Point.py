560d71254f4452ef1efc6784e89638b086789eab
S-Hanin <sign.rx@yandex.ru>
2020-04-23 17:44:24 +0300

    fix overload and incorrect call

--- CodeBattlePython/codebattleclient/internals/Point.py
+++ CodeBattlePython/codebattleclient/internals/Point.py
@@ -1,9 +1,10 @@
 class Point:
     """ Describes a point on board."""
+
     def __init__(self, x=0, y=0):
         self._x = int(x)
         self._y = int(y)
-    
+
     def __key(self):
         return self._x, self._y
 
@@ -29,41 +30,42 @@ class Point:
         return "[{},{}]".format(self._x, self._y)
 
     # Returns new BoardPoint object shifted bottom "delta" points
-    def shift_bottom(self, delta):
+    def shift_bottom_by(self, delta):
         return Point(self._x, self._y + delta)
 
     # Returns new BoardPoint object shifted bottom 1 point ???? надо ли todo
     def shift_bottom(self):
-        return self.shift_bottom(self, 1)
+        return self.shift_bottom_by(1)
 
     # Returns new BoardPoint object shifted top "delta" points
-    def shift_top(self, delta):
+    def shift_top_by(self, delta):
         return Point(self._x, self._y - delta)
 
     # Returns new BoardPoint object shifted top 1 point
     def shift_top(self):
-        return self.shift_top(self, 1)
+        return self.shift_top_by(1)
 
     # Returns new BoardPoint object shifted right to "delta" points
-    def shift_right(self, delta):
+    def shift_right_by(self, delta):
         return Point(self._x + delta, self._y)
 
     # Returns new BoardPoint object shifted right to 1 point
     def shift_right(self):
-        return self.shift_right(self, 1)
+        return self.shift_right_by(1)
 
     # Returns new BoardPoint object shifted left to "delta" points
-    def shift_left(self, delta):
+    def shift_left_by(self, delta):
         return Point(self._x - delta, self._y)
 
     # Returns new BoardPoint object shifted left to 1 point
     def shift_left(self):
-        return self.shift_left(1)
+        return self.shift_left_by(1)
 
     # Checks is current point on board or out of range.
     # @param boardSize Board size to compare
     def is_out_of_board(self, board_size):
         return self._x >= board_size or self._y >= board_size or self._x < 0 or self._y < 0
+
 
 if __name__ == '__main__':
     raise RuntimeError("This module is not expected to be ran from CLI")
